f01597d5a839ff287d7cda2839437abb5b970bc3
Carlos Portella <carlosportella16@hotmail.com>
2026-08-22 10:30:07 -0300

    feat: implementing jacoco test and sonarcube validation

--- core/src/test/java/com/onionmind/CoreApplicationTests.java
+++ core/src/test/java/com/onionmind/CoreApplicationTests.java
@@ -2,9 +2,9 @@ package com.onionmind;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import(TestcontainersConfiguration.class)
+@ActiveProfiles("test")
 @SpringBootTest
 class CoreApplicationTests {
 
